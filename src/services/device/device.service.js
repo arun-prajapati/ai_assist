7811140098c221e9c6c72baa3b1814f2c4f4d0b5
prem <prempanwala710@gmail.com>
2021-09-17 00:48:03 +0530

    dashboard changes

--- src/services/device/device.service.js
+++ src/services/device/device.service.js
@@ -4,12 +4,14 @@ import { logger, level } from "../../config/logger/logger";
 export const addDeviceHistoryData = async (deviceData) => {
   logger.log(level.info, "Services: addDeviceHistoryData");
   deviceData = JSON.parse(JSON.stringify(deviceData));
-  deviceData.date = new Date(moment().tz("Asia/calcutta").format());
+  deviceData.date = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/calcutta",
+  });
   deviceData.time = moment
     .tz(moment().format(), "Asia/calcutta")
     .format("hh:mm:ss");
   deviceData.deviceId = deviceData._id;
   delete deviceData._id;
-  console.log(">>afterdate modified", deviceData);
+  console.log(">>afterdate modified", deviceData.date);
   await deviceHistory.createData(deviceData);
 };
