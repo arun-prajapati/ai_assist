1888004f4ba7531ff96d6c98ebf068b82ceecd4f
prem <prempanwala710@gmail.com>
2021-09-21 10:35:22 +0530

    info:dashboard charts completed

--- src/controller/system/device/device.controller.js
+++ src/controller/system/device/device.controller.js
@@ -63,7 +63,7 @@ export const getSingleDevice = async (req, res, next) => {
       _id: req.params.deviceId,
     });
     var dates = new Date(moment().tz("Asia/calcutta").format());
-    //dates.setDate(dates.getDate() - 1);
+    dates.setDate(dates.getDate() - 1);
     dates.setHours(0, 0, 0);
     console.log(">>===", dates);
     let historyData = await deviceHistory.findData(
