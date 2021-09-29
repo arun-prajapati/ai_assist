bba37619c7370dcb5b4eb358610a423a9ad87176
prem <prempanwala710@gmail.com>
2021-09-29 14:21:03 +0530

    info:changes  in valve frame

--- src/services/device/device.service.js
+++ src/services/device/device.service.js
@@ -4,9 +4,7 @@ import { logger, level } from "../../config/logger/logger";
 export const addDeviceHistoryData = async (deviceData) => {
   logger.log(level.info, "Services: addDeviceHistoryData");
  // deviceData = JSON.parse(JSON.stringify(deviceData));
-  deviceData.date = new Date().toLocaleString("en-US", {
-    timeZone: "Asia/calcutta",
-  });
+  deviceData.date = new Date(moment().tz("Asia/calcutta").format("YYYY-MM-DD:h:m:s"));
   deviceData.time = moment
     .tz(moment().format(), "Asia/calcutta")
     .format("hh:mm:ss");
