9eda1572e66a79fda0ec9c4c93b800115e2426df
prem <prempanwala710@gmail.com>
2021-10-08 23:55:42 +0530

    info:changes

--- src/controller/system/Dashboard/dashboard.controller.js
+++ src/controller/system/Dashboard/dashboard.controller.js
@@ -14,7 +14,7 @@ import deviceHistory from "../../../models/deviceHistory.model";
 import { CONSTANTS as PERIOD_DATA } from "../../../constants/periodData";
 import moment from "moment";
 const mongoose = require("mongoose");
-let dates = new Date(moment.tz("Asia/calcutta"));
+let dates = new Date(moment.tz("Asia/calcutta").format("YYYY/MM/DD/hh/mm/ssz"));
 
 let dateData = {
   yy: dates.getFullYear(),
