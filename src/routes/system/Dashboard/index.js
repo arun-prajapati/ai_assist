ae0cd35009fdd995494efc2f28d58ef22a0563b3
prem <prempanwala710@gmail.com>
2021-10-11 14:15:41 +0530

    info:feat:added month statistics of graph

--- src/routes/system/Dashboard/index.js
+++ src/routes/system/Dashboard/index.js
@@ -3,14 +3,14 @@ import * as DashboardCtrl from "../../../controller/system/Dashboard/dashboard.c
 // import * as ErrorMiddleware from "../../../middleware/validatorError";
 // import { validate as DeviceValidate } from "../../../validator/system/device/device.validator";
 // import { CONSTANTS as SYSTEM_CONSTANTS } from "../../../constants/system/system";
-// import { AuthMiddleware } from "../../../middleware/authMiddleware";
+import { AuthMiddleware } from "../../../middleware/authMiddleware";
 const routes = new Router();
 const PATH = {
   ROOT: "/",
   DEVICECOUNTS: "/deviceCount",
   STATS: "/stats",
 };
-routes.route(PATH.DEVICECOUNTS).get(DashboardCtrl.deviceCount);
-routes.route(PATH.STATS).get(DashboardCtrl.graphData);
+routes.route(PATH.DEVICECOUNTS).get(AuthMiddleware, DashboardCtrl.deviceCount);
+routes.route(PATH.STATS).get(AuthMiddleware, DashboardCtrl.graphData);
 
 export default routes;
