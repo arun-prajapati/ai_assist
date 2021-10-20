62557358aaadb3a2819e12d50a9cca5b0be4f8e1
prem <prempanwala710@gmail.com>
2021-10-20 13:58:26 +0530

    changes in manual pump on/off

--- src/services/device/device.service.js
+++ src/services/device/device.service.js
@@ -3,13 +3,13 @@ import deviceHistory from "../../models/deviceHistory.model";
 import { logger, level } from "../../config/logger/logger";
 export const addDeviceHistoryData = async (deviceData) => {
   logger.log(level.info, "Services: addDeviceHistoryData");
- // deviceData = JSON.parse(JSON.stringify(deviceData));
+  // deviceData = JSON.parse(JSON.stringify(deviceData));
   deviceData.date = new Date().toLocaleString("en-US", {
     timeZone: "Asia/calcutta",
   });
   deviceData.time = moment
     .tz(moment().format(), "Asia/calcutta")
-    .format("hh:mm:ss");
+    .format("hh:mm:ss a");
   deviceData.deviceId = deviceData._id;
   delete deviceData._id;
   console.log(">>afterdate modified", deviceData.date);
