8e061ed6c7928a8815d36a301e67024755679e20
prem <prempanwala710@gmail.com>
2021-10-22 20:04:08 +0530

    info:changes in add device flow

--- src/cron-jobs/mailPdf.js
+++ src/cron-jobs/mailPdf.js
@@ -1,12 +1,13 @@
 import { scheduleJob } from "node-schedule";
 import { logger, level } from "../config/logger/logger";
 import moment from "moment";
+var schedule = require("node-schedule-tz");
 import Devices from "../models/device.model";
 import deviceHistory from "../models/deviceHistory.model";
 import * as DeviceSrv from "../services/device/device.service";
-const JOB_TIME = "05 20 * * *";
+const JOB_TIME = "07 20 * * *";
 const MIN = 15; // this minute ago data should be update
-scheduleJob(JOB_TIME, async () => {
+schedule.scheduleJob(JOB_TIME, "Asia/calcutta", async () => {
   try {
     logger.log(level.info, `>> PREM PANWALA at ${moment().format()}`);
     let data = await Devices.findData();
