cb599ae6dd43dced7e8990ab60e8a54beadb2e06
prem <prempanwala710@gmail.com>
2021-10-23 00:14:42 +0530

    info:changes in add device flow

--- src/cron-jobs/mailPdf.js
+++ src/cron-jobs/mailPdf.js
@@ -6,7 +6,7 @@ import Notifications from "../models/notification.model";
 import Devices from "../models/device.model";
 import deviceHistory from "../models/deviceHistory.model";
 import * as DeviceSrv from "../services/device/device.service";
-const JOB_TIME = "33 18 * * *";
+const JOB_TIME = "46 18 * * *";
 const mongoose = require("mongoose");
 const MIN = 15; // this minute ago data should be update
 scheduleJob(JOB_TIME, async () => {
@@ -36,7 +36,7 @@ scheduleJob(JOB_TIME, async () => {
         },
         {
           $match: {
-            deviceId: { $in: siteId },
+            // deviceId: { $in: siteId },
             date: {
               $gte: new Date(new Date(dates)),
               $lte: new Date(new Date(dates)).setHours(23, 59, 59),
