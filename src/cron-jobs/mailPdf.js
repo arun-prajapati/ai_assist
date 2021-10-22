a9080488c4e5518d5d45ece816639d5632418691
prem <prempanwala710@gmail.com>
2021-10-22 20:47:19 +0530

    info:changes in add device flow

--- src/cron-jobs/mailPdf.js
+++ src/cron-jobs/mailPdf.js
@@ -6,7 +6,7 @@ import Notifications from "../models/notification.model";
 import Devices from "../models/device.model";
 import deviceHistory from "../models/deviceHistory.model";
 import * as DeviceSrv from "../services/device/device.service";
-const JOB_TIME = "16 15 * * *";
+const JOB_TIME = "18 15 * * *";
 const MIN = 15; // this minute ago data should be update
 scheduleJob(JOB_TIME, async () => {
   try {
@@ -16,13 +16,14 @@ scheduleJob(JOB_TIME, async () => {
     for (let i = 0; i < notificationdata.length; i++) {
       siteId.push(notificationdata[i].siteId);
     }
-    // let deviceData = await Devices.findData(
-    //   {
-    //     _id: { $in: ["6155ae92ceceeb0036c3fde5", "615edffc321f51002b4bcd43"] },
-    //   },
-    //   {}
-    // );
+    let deviceData = await Devices.findData(
+      {
+        _id: { $in: siteId },
+      },
+      {}
+    );
     console.log("HIIII", siteId);
+    console.log("HIIII1", deviceData);
     logger.log(level.info, `>> PREM PANWALA at ${moment().format()}`);
   } catch (error) {
     logger.log(level.error, `>> Device state JOB error ${error}`);
