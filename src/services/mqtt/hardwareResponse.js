82ae94a5895a821ab2dc39b8007ede7039ac9167
prem <prempanwala710@gmail.com>
2021-10-25 15:46:33 +0530

    info:Remaining worksss

--- src/services/mqtt/hardwareResponse.js
+++ src/services/mqtt/hardwareResponse.js
@@ -390,7 +390,7 @@ export const VALVE_STATUS = async (macId, payload) => {
     if (((flowValue >> 15) & 1) == 1) {
       // flowValue = flowValue;
       // let flowValue = Int16Array(~flowValue);
-      console.log("Inside flow valuess", Int16Array(~flowValue));
+      console.log("Inside flow valuess", Int16Array(flowValue));
     }
     let deviceExist = await Devices.findOneDocument({ vmac: macId }); //findOne
     let deviceHistoryExist = await deviceHistory.isExist({ vmac: macId });
