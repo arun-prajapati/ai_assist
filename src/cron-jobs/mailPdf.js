fa265640c92a5d27362aee4b4b7b55051f1325e0
prem <prempanwala710@gmail.com>
2021-11-09 10:50:30 +0530

    fixing jobs

--- src/cron-jobs/mailPdf.js
+++ src/cron-jobs/mailPdf.js
@@ -6,7 +6,7 @@ import Notifications from "../models/notification.model";
 import Devices from "../models/device.model";
 import deviceHistory from "../models/deviceHistory.model";
 import * as DeviceSrv from "../services/device/device.service";
-const JOB_TIME = "07 05 * * *";
+const JOB_TIME = "22 05 * * *";
 const mongoose = require("mongoose");
 const CsvParser = require("json2csv").Parser;
 const MIN = 15; // this minute ago data should be update
@@ -50,6 +50,7 @@ scheduleJob(JOB_TIME, async () => {
         {
           $project: {
             date: { $last: "$totaliser.totaliser_current_value" },
+            name: 1,
           },
         },
         // { $sort: { date: -1 } },
