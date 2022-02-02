550c34940b4976b5992cd3ea6f51b2212a0418e9
PremPanwala <prempanwala710@gmail.com>
2022-02-02 17:55:59 +0530

    chnages done of module

--- src/controller/system/Dashboard/dashboard.controller.js
+++ src/controller/system/Dashboard/dashboard.controller.js
@@ -212,7 +212,10 @@ export const graphData = async (req, res, next) => {
       console.log("Inside Day", mergeArrayResponse.length - 1);
       console.log("mid night base", midnightBase);
       for (let i = mergeArrayResponse.length - 1; i >= 0; i--) {
-        var dates2223 = new Date(moment().tz("Asia/calcutta"));
+        var dates2223 = new Date(
+          moment().tz("Asia/calcutta").format("YYYY/MM/DD hh:mm:ss")
+        );
+        console.log("dates2223", dates2223);
         console.log("dates2223", dates2223.getHours());
         console.log("dates2223", typeof dates2223.getHours());
         if (
