c2553cebc460456263c956280b4ce1db3861546b
PremPanwala <prempanwala710@gmail.com>
2022-02-02 21:26:04 +0530

    chnages done of module

--- src/controller/system/Dashboard/dashboard.controller.js
+++ src/controller/system/Dashboard/dashboard.controller.js
@@ -111,21 +111,8 @@ export const graphData = async (req, res, next) => {
   try {
     let pipeline;
     if (req.query.type === "day") {
-      let historyData1 = deviceHistory.findData(
-        {
-          deviceId: mongoose.Types.ObjectId(req.query.deviceId),
-          date: {
-            $gte: new Date(new Date(dates222)),
-            $lte: new Date(new Date(dates222).setHours(23, 59, 59)),
-          },
-          totaliser_current_value: {
-            $gte: midnightBase,
-          },
-        },
-        { createdAt: 0 },
-        { sort: { date: -1 }, limit: 1 }
-      );
       let midnightBase = 0;
+      let historyData1;
       var dates1 = new Date(moment().tz("Asia/calcutta").format("YYYY-MM-DD"));
       dates1.setDate(dates1.getDate() - 1);
       //dates.setHours(0, 0, 0);
@@ -191,7 +178,20 @@ export const graphData = async (req, res, next) => {
           { $sort: { _id: 1 } },
         ];
         midnightBase = historyData[0].totaliser_current_value;
-
+        historyData1 = deviceHistory.findData(
+          {
+            deviceId: mongoose.Types.ObjectId(req.query.deviceId),
+            date: {
+              $gte: new Date(new Date(dates222)),
+              $lte: new Date(new Date(dates222).setHours(23, 59, 59)),
+            },
+            totaliser_current_value: {
+              $gte: midnightBase,
+            },
+          },
+          { createdAt: 0 },
+          { sort: { date: -1 }, limit: 1 }
+        );
         graphData = await deviceHistory.aggregate(pipeline);
         console.log("Hours graph Data", graphData);
         // graphData = JSON.parse(JSON.stringify(graphData));
