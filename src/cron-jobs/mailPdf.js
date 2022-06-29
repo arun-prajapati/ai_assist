7d504c520a385cd011f3bc2c62b31208cedcd207
prem panwala <prem.panwala@bacancy.com>
2022-06-29 12:18:50 +0530

    DOne with changes

--- src/cron-jobs/mailPdf.js
+++ src/cron-jobs/mailPdf.js
@@ -6,7 +6,7 @@ import Notifications from "../models/notification.model";
 import Devices from "../models/device.model";
 import deviceHistory from "../models/deviceHistory.model";
 import * as DeviceSrv from "../services/device/device.service";
-const JOB_TIME = "30 06 * * *";
+const JOB_TIME = "30 17 * * *";
 const mongoose = require("mongoose");
 const CsvParser = require("json2csv").Parser;
 const MIN = 15; // this minute ago data should be update
