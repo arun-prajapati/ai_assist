3d830092ca75b1246a0cb9977a71938fd2ea6e97
prem panwala <prem.panwala@bacancy.com>
2023-02-05 15:26:32 +0530

    deployment

--- src/validator/user/user.validator.js
+++ src/validator/user/user.validator.js
@@ -23,7 +23,7 @@ export const validate = (method) => {
           .not()
           .isEmpty()
           .trim()
-          .isLength({ min: 10, max: 10 }),
+          .isLength({ min: 12, max: 12 }),
         body("gender", "gender should be valid")
           .not()
           .isEmpty()
